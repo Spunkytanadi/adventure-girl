164af355964724a5895d34555c115f57c59be569
Marceilla Norman <101609514+Spunkytanadi@users.noreply.github.com>
2022-07-09 09:39:43 -0500

    main

---
+++ index.js
@@ -0,0 +1,23 @@
+const character = document.getElementById("character");
+const monster = document.getElementById("monster");
+
+const jump = function () {
+  if(character.classList!=='jump'){
+    character.classList.add('jump');
+  }
+  setTimeout(function(){
+    character.classList.remove('jump');
+  }, 300)
+}
+
+let collision = setInterval(function(){
+  let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
+  let monsterLeft = parseInt(window.getComputedStyle(monster).getPropertyValue('left'));
+  if(monsterLeft<10 && monsterLeft > 0 && characterBottom >= 100){
+    alert('Game Over');
+  }
+}, 10);
+
+document.addEventListener("keydown", function(event){
+  jump()
+})
